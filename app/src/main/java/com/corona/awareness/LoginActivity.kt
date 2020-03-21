d0fe7dfe7fd5ec2fc391156aa23ad1359d2a2b81
Nabil Sunesara <nabilsunesara@outlook.com>
2020-03-21 15:14:51 +0400

    Adding Sign up

--- app/src/main/java/com/corona/awareness/LoginActivity.kt
+++ app/src/main/java/com/corona/awareness/LoginActivity.kt
@@ -1,17 +1,26 @@
 package com.corona.awareness
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
 import com.corona.awareness.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : BaseActivity() {
 
     private lateinit var bindingView: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        bindingView = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        bindingView = setContentViewDataBinding(R.layout.activity_login)
+        setupUI()
     }
 
+    private fun setupUI() {
+        bindingView.signUpBtn.setOnClickListener {
+            goToSignUpActivity()
+        }
+    }
 
+    private fun goToSignUpActivity() {
+        val intent = Intent(this, SignUpActivity::class.java)
+        startActivity(intent)
+    }
 }
