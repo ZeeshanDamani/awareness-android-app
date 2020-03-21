c375e06e262789b6305d1ad172cacf3c12e21367
Zeeshan Damani <zeeshanmoizdamani@gmail.com>
2020-03-21 16:42:20 +0500

    Added Authentication and dashboard (#2)
    * Added Authentication and dashboard
    
    * Rebase Authentication into latest branch
    
    * Rebase Authentication into latest branch
    
    Co-authored-by: Zeeshan Moiz Damani <zeeshan.damani@careem.com>

--- app/src/main/java/com/corona/awareness/SignUpActivity.kt
+++ app/src/main/java/com/corona/awareness/SignUpActivity.kt
@@ -111,16 +111,6 @@ class SignUpActivity : BaseActivity() {
         }
     }
 
-    private data class SignUpData(
-        val firstName: String,
-        val lastName: String,
-        val phoneNumber: String,
-        val cnic: String,
-        val password: String,
-        val passwordConfirmation: String,
-        val email: String,
-        val dateOfBirth: String
-    )
 
     private enum class ValidationResult {
         VALID,
